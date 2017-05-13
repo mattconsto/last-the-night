4d0cced8f2023d2b9254a49e034fff4e831bc6f3
U-Z-DELL-E7450\User <User@Z-Dell-E7450.ecs.soton.ac.uk>
2017-05-13 02:22:05 +0100

    Running and sound effects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    public AudioSource runningSource;
+    public AudioSource effectSource;
+
+    public AudioClip jumpClip;
+    public AudioClip stepClip;
+    public AudioClip torchClip;
+
 	private Rigidbody _rb;
     private float _canJump = 0;
 
@@ -13,6 +20,8 @@ public class PlayerController : MonoBehaviour {
 	public GameObject cam;
 	public GameObject torch;
 
+    private float _distance;
+
 	public void Start () {
 		_rb = GetComponent<Rigidbody>();
 	}
@@ -21,18 +30,38 @@ public class PlayerController : MonoBehaviour {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
 
-		_rb.AddForce(transform.right * Input.GetAxis("Horizontal") * speed, ForceMode.Impulse);
-		_rb.AddForce(transform.forward * Input.GetAxis("Vertical") * speed, ForceMode.Impulse);
+        // Running
+        float multiplier = Input.GetButton("Fire3") ? 2 : 1;
+        if(Input.GetButton("Fire3")) {
+            multiplier = 2;
+            runningSource.volume = Mathf.Lerp(runningSource.volume, 0.75f, 0.005f);
+        } else {
+            runningSource.volume = Mathf.Lerp(runningSource.volume, 0, 0.1f);
+        }
+
+        // Footsteps
+        if(_canJump > 0) {
+            _distance += (Input.GetAxis("Horizontal") + Input.GetAxis("Vertical")) * multiplier;
+            if(_distance > 50) {
+                effectSource.PlayOneShot(stepClip, 0.25f);
+                _distance = 0;
+            }
+        }
+
+		_rb.AddForce(transform.right * Input.GetAxis("Horizontal") * speed * multiplier, ForceMode.Impulse);
+		_rb.AddForce(transform.forward * Input.GetAxis("Vertical") * speed * multiplier, ForceMode.Impulse);
 		transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * sensitivity);
 		cam.transform.Rotate(Vector3.left * Input.GetAxis("Mouse Y") * sensitivity);
         cam.transform.localEulerAngles = new Vector3((Mathf.Clamp((cam.transform.localEulerAngles.x + 90) % 360, 50, 130) + 270) % 360, 0, 0);
 
         if(Input.GetButton("Jump") && _canJump > 0) {
             _rb.AddForce(transform.up * jump);
+            effectSource.PlayOneShot(jumpClip, 0.5f);
         }
 
         if(Input.GetButtonDown("Submit")) {
             torch.SetActive(!torch.activeSelf);
+            effectSource.PlayOneShot(torchClip, 0.5f);
         }
 
         #if !UNITY_EDITOR
