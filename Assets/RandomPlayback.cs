5df9495a125ece2ffac9a8353be5d923b7bee625
U-Z-DELL-E7450\User <User@Z-Dell-E7450.ecs.soton.ac.uk>
2017-05-13 18:47:52 +0100

    Fade in ambient noise from a random position, improve chunk generation

---
+++ Assets/RandomPlayback.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPlayback : MonoBehaviour {
+	public float fadeTime = 1f;
+	public bool random = true;
+
+	private float _timer = 0;
+	private AudioSource _source;
+
+	public void Start() {
+		_source = GetComponent<AudioSource>();
+
+		if(_source != null) {
+			if(random) _source.time = new System.Random().NextFloat(_source.clip.length);
+			_source.volume = 0;
+			_source.Play();
+			_timer = fadeTime;
+		}
+	}
+
+	public void Update() {
+		_timer -= Time.deltaTime;
+		if(_source != null) _source.volume = 1 - _timer / fadeTime;
+		if(_timer < 0) enabled = false;
+	}
+}
