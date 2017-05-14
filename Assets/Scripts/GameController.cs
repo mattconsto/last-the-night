1e05d201cd3a2c32239337b0819ee27e42c28f89
U-Z-DELL-E7450\User <User@Z-Dell-E7450.ecs.soton.ac.uk>
2017-05-14 21:05:45 +0100

    Improve HUD

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -17,9 +17,22 @@ public class GameController : MonoBehaviour {
 		hud = GetComponent<HUDController>();
 	}
 
+	public void Update() {
+		if(Input.GetButtonDown("Cancel")) {
+			if(state == State.PLAY) {
+				Pause();
+			} else if(state == State.PAUSE) {
+				Resume();
+			}
+		}
+
+		if(!player.gameObject.activeSelf) {
+			End();
+		}
+	}
+
 	public void Begin() {
 		if(state != State.PLAY) {
-			state = State.PLAY;
 			hud.ToggleHUDs();
 			player.enabled = true;
 			Cursor.visible = false;
@@ -27,17 +40,42 @@ public class GameController : MonoBehaviour {
 			generator.config.seed = seed.text.Length > 0 ? Convert.ToInt32(seed.text) : 0;
 			generator.enabled = true;
 			player.GetComponent<Rigidbody>().isKinematic = false;
+			transform.Find("HUDs/Title HUD/Menu/Start").gameObject.SetActive(false);
+			transform.Find("HUDs/Title HUD/Menu/Resume").gameObject.SetActive(true);
+			state = State.PLAY;
+		}
+	}
+
+	public void Pause() {
+		if(state != State.PAUSE) {
+			hud.ToggleHUDs();
+			Cursor.visible = true;
+			Cursor.lockState = CursorLockMode.None;
+			Time.timeScale = 0;
+			state = State.PAUSE;
+		}
+	}
+
+	public void Resume() {
+		if(state != State.PLAY) {
+			hud.ToggleHUDs();
+			Cursor.visible = false;
+			Cursor.lockState = CursorLockMode.Locked;
+			Time.timeScale = 1;
+			state = State.PLAY;
 		}
 	}
 
 	public void End() {
 		if(state != State.END) {
-			state = State.END;
 			hud.ToggleHUDs();
 			player.enabled = false;
 			Cursor.visible = true;
 			Cursor.lockState = CursorLockMode.None;
 			generator.enabled = false;
+			transform.Find("HUDs/Title HUD/Menu/Resume").gameObject.SetActive(false);
+			transform.Find("HUDs/Title HUD/Menu/Restart").gameObject.SetActive(true);
+			state = State.END;
 		}
 	}
 }
