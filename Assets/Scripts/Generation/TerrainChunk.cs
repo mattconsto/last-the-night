7948c2a1dc5f0e2848a2fe0e9862368f33308cd4
U-Z-DELL-E7450\User <User@Z-Dell-E7450.ecs.soton.ac.uk>
2017-05-14 11:56:28 +0100

    Add more prefabs, framerate is shit xD

--- Assets/Scripts/Generation/TerrainChunk.cs
+++ Assets/Scripts/Generation/TerrainChunk.cs
@@ -9,6 +9,9 @@ public class TerrainChunk : ScriptableObject {
 	public LODMesh[] lodMeshs;
 	public MapData map;
 	public GameObject gameObject;
+	public GameObject treeContainer;
+	public GameObject grassContainer;
+	public GameObject structureContainer;
 	public bool recieved = false;
 
 	private bool _visible;
@@ -23,7 +26,7 @@ public class TerrainChunk : ScriptableObject {
 		set {_visible = value; gameObject.SetActive(_visible);}
 	}
 
-	public void init(Vector2 coord, int size, Transform parent, GenerationConfig config) {
+	public void init(Vector2 coord, int size, GameObject parent, GenerationConfig config) {
 		this.config = config;
 
 		position = coord * (size - 2*2*4);
@@ -34,12 +37,20 @@ public class TerrainChunk : ScriptableObject {
 
 		gameObject = new GameObject("Terrain Chunk");
 		gameObject.tag = "Jumpable";
+		gameObject.transform.position = new Vector3(position.x, 0, position.y);
+		gameObject.transform.parent = parent.transform;
+		treeContainer = new GameObject("Tree Container");
+		treeContainer.transform.parent = gameObject.transform;
+		grassContainer = new GameObject("Grass Container");
+		grassContainer.transform.parent = gameObject.transform;
+		structureContainer = new GameObject("Structure Container");
+		structureContainer.transform.parent = gameObject.transform;
+
 		_mf = gameObject.AddComponent<MeshFilter>();
 		_mr = gameObject.AddComponent<MeshRenderer>();
 		_mc = gameObject.AddComponent<MeshCollider>();
 		_mr.material = new Material(config.material);
-		gameObject.transform.position = new Vector3(position.x, 0, position.y);
-		gameObject.transform.parent = parent;
+
 		visible = false;
 
 		config.generator.RequestMapData(OnMapReceived, config, position / size);
@@ -65,12 +76,43 @@ public class TerrainChunk : ScriptableObject {
 
 			if(map.noise[y, x] > 0.6f && map.noise[y, x] < 0.8f) {
 				GameObject tree = Instantiate(config.treePrefabs[rng.Next(config.treePrefabs.Length)]);
-				tree.transform.parent = gameObject.transform;
+				tree.transform.parent = treeContainer.transform;
 				tree.transform.position = new Vector3(
-					bounds.center.x + 0 - bounds.size.x * ((float) (map.noise.GetLength(1)-y) / map.noise.GetLength(1) - 0.5f),
-					config.curve.Evaluate(map.noise[y, x]) * 40, // TODO remove horrible magic number
-					bounds.center.y + bounds.size.y * ((float) (map.noise.GetLength(0)-x) / map.noise.GetLength(0) - 1.5f) // Not sure why it needs to be minus 1
+					bounds.center.x + 0 - bounds.size.x * ((float) (map.noise.GetLength(1) - y) / map.noise.GetLength(1) - 0.5f),
+					config.curve.Evaluate(map.noise[y, x]) * config.scale.y / 1.5f,
+					bounds.center.y + bounds.size.y * ((float) (map.noise.GetLength(0) - x) / map.noise.GetLength(0) - 1.5f) // Not sure why it needs to be minus 1
 				);
+				tree.transform.rotation = Quaternion.Euler(0, rng.NextFloat(360), 0);
+			}
+		}
+
+		for(int i = 0; i < 10; i++) {
+			int y = rng.Next(map.noise.GetLength(0)), x = rng.Next(map.noise.GetLength(1));
+
+			if(map.noise[y, x] > 0.5f && map.noise[y, x] < 0.85f) {
+				GameObject grass = Instantiate(config.flowerPrefabs[rng.Next(config.flowerPrefabs.Length)]);
+				grass.transform.parent = grassContainer.transform;
+				grass.transform.position = new Vector3(
+					bounds.center.x + 0 - bounds.size.x * ((float) (map.noise.GetLength(1) - y) / map.noise.GetLength(1) - 0.5f),
+					config.curve.Evaluate(map.noise[y, x]) * config.scale.y / 1.25f,
+					bounds.center.y + bounds.size.y * ((float) (map.noise.GetLength(0) - x) / map.noise.GetLength(0) - 1.5f) // Not sure why it needs to be minus 1
+				);
+				grass.transform.rotation = Quaternion.Euler(0, rng.NextFloat(360), 0);
+			}
+		}
+
+		for(int i = 0; i < 1; i++) {
+			int y = rng.Next(map.noise.GetLength(0)), x = rng.Next(map.noise.GetLength(1));
+
+			if(map.noise[y, x] > 0.4f && map.noise[y, x] < 0.6f) {
+				GameObject structure = Instantiate(config.structurePrefabs[rng.Next(config.structurePrefabs.Length)]);
+				structure.transform.parent = structureContainer.transform;
+				structure.transform.position = new Vector3(
+					bounds.center.x + 0 - bounds.size.x * ((float) (map.noise.GetLength(1) - y) / map.noise.GetLength(1) - 0.5f),
+					config.curve.Evaluate(map.noise[y, x]) * config.scale.y,
+					bounds.center.y + bounds.size.y * ((float) (map.noise.GetLength(0) - x) / map.noise.GetLength(0) - 1.5f) // Not sure why it needs to be minus 1
+				);
+				structure.transform.rotation = Quaternion.Euler(0, rng.NextFloat(360), 0);
 			}
 		}
 	}
@@ -90,6 +132,9 @@ public class TerrainChunk : ScriptableObject {
 			}
 
 			if(index != previousLOD) {
+				grassContainer.SetActive(config.lods[index].lod <= 4);
+				treeContainer.SetActive(config.lods[index].lod <= 8);
+				structureContainer.SetActive(config.lods[index].lod <= 16);
 				LODMesh mesh = lodMeshs[index];
 				if(mesh.recieved) {
 					previousLOD = index;
