d4a0ba09c94d8b13e2fb213d33e730ec2a147446
Matt Consto <mrconsterdine@googlemail.com>
2017-05-15 13:35:50 +0100

    Add initial random seed

---
+++ Assets/Scripts/UI/InitialSeedCreator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InitialSeedCreator : MonoBehaviour {
+	public void Start () {
+		GetComponent<InputField>().text = "" + new System.Random().Next(0, 10000000);
+	}
+}
