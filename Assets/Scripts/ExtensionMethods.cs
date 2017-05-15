bfec22cd74ac54033a34e31fb8ed488584f2afe6
Matt Consto <mrconsterdine@googlemail.com>
2017-05-15 11:39:00 +0100

    Prune prefabs before generation to provide variety

--- Assets/Scripts/ExtensionMethods.cs
+++ Assets/Scripts/ExtensionMethods.cs
@@ -3,6 +3,17 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public static class ExtensionMethods {
+	/* Arrays */
+
+	public static T[] RemoveAt<T>(this T[] source, int index) {
+	    T[] dest = new T[source.Length - 1];
+	    
+	    if(index > 0) Array.Copy(source, 0, dest, 0, index);
+	    if(index < source.Length - 1) Array.Copy(source, index + 1, dest, index, source.Length - index - 1);
+
+	    return dest;
+	}
+
 	/* Random Extensions */
 
 	public static double NextDouble(this System.Random r, double a) {
